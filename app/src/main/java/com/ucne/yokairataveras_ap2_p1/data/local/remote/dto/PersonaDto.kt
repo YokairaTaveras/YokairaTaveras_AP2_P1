1f5a01215be32ba0c98ed99595965ac30760aa91
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2024-06-17 23:56:04 -0400

    Agregando PersonaDto

---
+++ app/src/main/java/com/ucne/yokairataveras_ap2_p1/data/local/remote/dto/PersonaDto.kt
@@ -0,0 +1,8 @@
+package com.ucne.yokairataveras_ap2_p1.data.local.remote.dto
+
+data class PersonaDto (
+    val personaId: Int,
+    val nombre: String,
+    val apellido: String,
+    val direccion: String
+)
